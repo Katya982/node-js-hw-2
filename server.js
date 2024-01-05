699d1829d08c8262de8d737f14b9ba56785143e5
Katya982 <xamko98@gmail.com>
2024-01-05 14:13:08 +0200

    add hw 3

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+import mongoose from 'mongoose';
+
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://Katya:hGPSsOxecaJXWSZK@cluster0.2bvc2tj.mongodb.net/my-contacts?retryWrites=true&w=majority";
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+
+// hGPSsOxecaJXWSZK - code
